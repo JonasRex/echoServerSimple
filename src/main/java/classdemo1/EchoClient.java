172cd5ac92a841e7f50b99e2969b1fa058aa5f78
JonasRex <78900583+JonasRex@users.noreply.github.com>
2021-09-21 19:52:37 +0200

    Update EchoClient.java

--- src/main/java/classdemo1/EchoClient.java
+++ src/main/java/classdemo1/EchoClient.java
@@ -6,7 +6,30 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class EchoClient {
-    public static void main(String[] args) {
+    Socket socket;
+    PrintWriter pw;
+    Scanner scanner;
 
+    public void connect(String address, int port) throws IOException {
+        socket = new Socket(address, port);
+        pw = new PrintWriter(socket.getOutputStream(), true);
+        scanner = new Scanner(socket.getInputStream());
+        System.out.println(scanner.nextLine());
+
+        Scanner keyboard = new Scanner(System.in);
+        boolean keepRunning = true;
+        while (keepRunning){
+            String msgToSend = keyboard.nextLine();
+            pw.println(msgToSend);
+            System.out.println(scanner.nextLine());
+            if (msgToSend.equals("stop")){
+                keepRunning = false;
+            }
+        }
+        socket.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        new EchoClient().connect("localhost", 2345);
     }
 }
